7774d632d411210b639c2444b7afe6940e55b549
SergeLys <lisekno.1996@mail.ru>
2019-08-06 10:52:01 +0300

    xml refactoring and new icons

--- app/src/main/java/com/squares/core/Data.java
+++ app/src/main/java/com/squares/core/Data.java
@@ -5,4 +5,11 @@ public class Data {
     public static final String APP_PREFERENCES = "mysettings";
     public static final String APP_COUNTER = "counter";
 
+
+    /*1) Разобраться ч икноками и файлами ic_launcher
+            2) Проыерить на утечку памяти
+            3) Обновить API
+            4) Подключить рекламу
+            5) Проверить игру
+            6) В GameActivity посмотреть :78*/
 }
