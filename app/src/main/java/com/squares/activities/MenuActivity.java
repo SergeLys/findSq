10e36d638c4c6a02bf9fe32c8350092fefe7c8bc
SergeLys <lisekno.1996@mail.ru>
2019-08-08 12:49:14 +0300

    An admob adding

--- app/src/main/java/com/squares/activities/MenuActivity.java
+++ app/src/main/java/com/squares/activities/MenuActivity.java
@@ -2,20 +2,28 @@ package com.squares.activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.squares.R;
 
 public class MenuActivity extends Activity {
 
-    private static SharedPreferences mSettings;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        MobileAds.initialize(this, "ca-app-pub-2633165848097716~1040051130");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
     public void clickStartBtn(View view){
